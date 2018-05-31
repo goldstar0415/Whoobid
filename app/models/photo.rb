c4366c5dcabe5c6fc2a3da886c0df41938482bf6
goldenprovider0415 <topdeveloper1212@gmail.com>
2018-05-31 05:18:50 +0000

    init

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -3,7 +3,7 @@ class Photo < ApplicationRecord
 	mount_uploader :main_photo, MainPhotoUploader # Uploader for this model
 	mount_uploader :second_photo, SecondPhotoUploader # Uploader for this model
 	mount_uploader :third_photo, ThirdPhotoUploader # Uploader for this model
-	mount_uploader :fourth_photo, FourthoPhotUploader # Uploader for this model
+	mount_uploader :fourth_photo, FourthPhotoUploader # Uploader for this model
 	#Association
 	belongs_to :auction, :inverso_of => :auctions
 end
